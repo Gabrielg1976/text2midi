842da0f10a32e3c5bbb0e4fb0acfd89ae87b48e2
Gabriel Garrod <gabrielg1976@gmail.com>
2009-01-15 21:29:55 -0800

    added the arpeggation synthcontroller and a basic synth html.erb page for the view

---
+++ text2midi/app/controllers/arpeg_synth_controller.rb
@@ -0,0 +1,10 @@
+class ArpegSynthController < ApplicationController
+    
+    def arp_synth
+        
+    end
+    
+    
+    
+    
+end
--- text2midi/config/routes.rb
+++ text2midi/config/routes.rb
@@ -5,6 +5,7 @@ ActionController::Routing::Routes.draw do |map|
  map.resource :session
  map.resources :example
  map.root :controller => "example", :action=>"write_text"
+ map.resource :contoller =>"arpeg_synth",:action=>"arp_synth"
  map.connect ':controller/:action/:id'
  map.connect ':controller/:action/:id.:format'
 end
